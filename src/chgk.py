d5073b5795614e0b3d5da087d94464138601a8f9
aidanagul10 <aidanagul10@gmail.com>
2021-03-02 10:03:51 +0500

    Добавлен парсер ЧГК

--- src/chgk.py
+++ src/chgk.py
@@ -1,5 +1,9 @@
 from abc import ABCMeta, abstractmethod
 from typing import Tuple, List
+import aiohttp
+import asyncio
+from lxml import html
+import re
 
 
 class Question:
@@ -22,7 +26,7 @@ class Question:
     @abstractmethod
     def answer_text(self) -> str:
         """Текст ответа, выводимый пользователю."""
-    
+
     @abstractmethod
     def check_answer(self, answer: str) -> bool:
         """Проверка ответа"""
@@ -53,27 +57,87 @@ class DummyQuestion(Question):
         self._id = id
         self._question = question
         self._answer = answer
-    
+
     def id(self) -> str:
         return self._id
+
     def question(self) -> str:
         return self._question
+
     def answer(self) -> str:
         return self._answer
+
     def check_answer(self, answer: str) -> bool:
         return self.answer() == answer
+
 
 class DummyQuestionStorage(QuestionStorage):
     def __init__(self, total):
         self._total = total
 
     async def get_by_id(self, id: str) -> Question:
-        return DummyQuestion(id, 'question'+id, 'answer'+id)
-    
+        return DummyQuestion(id, 'question' + id, 'answer' + id)
+
     async def find(self, content: str, page: int, page_size: int) -> Tuple[int, List[str]]:
         start = page * page_size
         end = start + page_size
         if end > self._total:
             end = self._total
         return self._total, [await self.get_by_id(str(i)) for i in range(start, end)]
+
+
+class CHGKQuestion(Question):
+    def __init__(self, id, question, answer):
+        self._id = id
+        self._question = question
+        self._answer = answer
+
+    def id(self) -> str:
+        return self._id
+
+    def question_text(self) -> str:
+        return self._question
+
+    def answer_text(self) -> str:
+        return self._answer
+
+    def check_answer(self, answer: str) -> bool:
+        if answer == self._answer:
+            return True
+        return False
+
+
+class CHGKQuestionStorage(QuestionStorage):
+    async def find(self, content: str, page: int, page_size: int) -> Tuple[int, List[str]]:
+        async with aiohttp.ClientSession() as session:
+            url = f'https://db.chgk.info/search/questions/{content}/types123/limit{page_size}?page={page}'
+            async with session.get(url) as response:
+                result = await response.text()
+                return self.parse_result(result)
+
+    def parse_result(self, content):
+        tree = html.fromstring(content)
+        elems = tree.xpath('//h2[@class="title"]/text()')[1]
+        total = re.findall(r'\d+', elems)
+
+        elems = tree.xpath('//dl[@class="search-results questions-results"]//dd/div[@class="question"]//strong[@class="Question"]//a/@href')
+        return int(total[0]), list(map(lambda x: x.replace('/question/', ''), elems))
+
+    async def get_by_id(self, id: str) -> Question:
+        async with aiohttp.ClientSession() as session:
+            url = f'https://db.chgk.info/question/{id}'
+            async with session.get(url) as response:
+                content = await response.text()
+                quest_res = self.parse_question(content)[0]
+                answer = self.parse_question(content)[1]
+                quest = CHGKQuestion(id, quest_res, answer)
+                return quest
+
+    def parse_question(self, content) -> Question:
+        tree = html.fromstring(content)
+        elems = tree.xpath('//p/text()')
+        return elems[1].replace('\n', ' ').strip(), elems[3].strip()
+
+
+
 
--- src/test_chgk.py
+++ src/test_chgk.py
@@ -1,5 +1,7 @@
 import asyncio
-from chgk import DummyQuestionStorage
+import string
+
+from chgk import DummyQuestionStorage, CHGKQuestionStorage
 
 
 # Простой вариант
@@ -23,3 +25,32 @@ async def test_dummy_find():
     assert total == len(questions)
     for i, quest in enumerate(questions):
         assert quest.id() == str(i)
+
+
+@async_test
+async def test_chgk_get_by_id():
+    id = 'gerbr13/98'
+    qs = CHGKQuestionStorage()
+    question = await qs.get_by_id(id)
+    assert question.id() == id
+    s = 'В 2002 году в ходе одной церемонии над Виндзором пролетел самолет времен Второй мировой войны'
+    assert question.question_text().find(s) != -1
+
+
+@async_test
+async def test_chgk_find():
+    content = 'море'
+    page = 0
+    page_size = 4
+    qs = CHGKQuestionStorage()
+    quest = await qs.find(content, page, page_size)
+    total = 5150
+    id_list = ['leti08.4/4', 'rubr08st/154', 'ukrbr07.7/3', 'uzbek17.4/10']
+    assert quest[1] == id_list
+    assert quest[0] == total
+
+
+
+@async_test
+async def test_chgk_check_answer():
+    pass
